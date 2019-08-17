0cc49663b032511bf969f59335dfc151463ebb05
Jake Drake <fingerblasterstudios@gmail.com>
2019-08-16 23:10:54 -0700

    Added command line argument support

--- galaxyPopulator.py
+++ galaxyPopulator.py
@@ -1,30 +1,46 @@
 import random
+import sys
 from starData import starData
 from starData import standardHeader
 
 
+
+
 #<star name="Sol" temp="100" x="0" y="0" size="1.0" numPlanets="0" numGasGiants="0" >
 
-starDataList = starData.split("\n")
-starNames = random.sample(starDataList, 200)
 
 def genStarTag(name="unnamed-star", temp=100, x=0, y=0, size=1.0, numPlanets=5, numGasGiants=2, blackHole="false"):
     starTag = '\t<star name="%s" temp="%s" x="%s" y="%s" size="%s" numPlanets="%s" numGasGiants="%s" blackHole="%s">\n\t</star>\n' % (name, temp, x, y, size, numPlanets, numGasGiants, blackHole)
     return starTag
 
-output = standardHeader % (2,1)
+numStars = int(sys.argv[1])
+minStarSize = float(sys.argv[2])
+maxStarSize = float(sys.argv[3])
+minStarTemp = int(sys.argv[4])
+maxStarTemp = int(sys.argv[5])
+minPlanets = int(sys.argv[6])
+maxPlanets = int(sys.argv[7])
+minGasGiants = int(sys.argv[8])
+maxGasGiants = int(sys.argv[9])
+blackHolePct = int(sys.argv[10])
+
+starDataList = starData.split("\n")
+starNames = random.sample(starDataList, numStars)
+
+output = standardHeader % (random.randint(minPlanets,maxPlanets), random.randint(minGasGiants,maxGasGiants))
+
+
 
 for name in starNames:
-    temp = random.randint(50, 200)
+    temp = random.randint(minStarTemp, maxStarTemp)
     x = random.randint(-1000, 2500)
     y = random.randint(-1000, 2000)
-    size = random.uniform(0.12, 2.5)
-    numPlanets = random.randint(0,2)
-    numGasGiants = random.randint(0,1)
-
+    size = random.uniform(minStarSize, maxStarSize)
+    numPlanets = random.randint(minPlanets,maxPlanets)
+    numGasGiants = random.randint(minGasGiants,maxGasGiants)
     blackHoleChance = random.randint(0,100)
 
-    if blackHoleChance > 1:
+    if blackHoleChance > blackHolePct:
         blackHole = "false";
     else:
         blackHole = "true";
