71c36ed193c2541fcca59932f2203f36061635cf
Jake Drake <fingerblasterstudios@gmail.com>
2019-08-18 09:45:11 -0700

    fixed every star havein the same number of planets and each planet in a star system having the same number of moons

--- galaxyPopulator.py
+++ galaxyPopulator.py
@@ -5,19 +5,18 @@ import math
 
 from config import *
 
+# class for easily passing data between the various generation functions
 class dataBlock:
     def __init__(self, data="", ID=0):
         self.data = data
         self.ID = ID
         self.pSize = 0
 
-#<star name="Sol" temp="100" x="0" y="0" size="1.0" numPlanets="0" numGasGiants="0" >
-
-
+# closing tags
 closeStarTag = "\t</star>\n"
 closePlanetTag = "\t\t</planet>\n"
-oBlock ="\t\t\t\t<oceanBlock>%s</oceanBlock>\n"
 
+# taken from https://stackoverflow.com/questions/783897/truncating-floats-in-python
 def truncate(f, n):
     '''Truncates/pads a float f to n decimal places without rounding'''
     s = '{}'.format(f)
@@ -26,11 +25,12 @@ def truncate(f, n):
     i, p, d = s.partition('.')
     return '.'.join([i, (d+'0'*n)[:n]])
 
+# generate a star tag, append it to the block, and return the block
 def genStarTag(block, name="unnamed-star", temp=100, x=0, y=0, size=1.0, blackHole="false"):
     block.data += starData.starTag % (name, temp, x, y, size, blackHole)
     return block
 
-
+# generate a planet tag, append it to the block, and return the block
 def genPlanetTag(block, name, rings="false", gasGiant="false", aDens=100, gMul=100, oDist=100, oTheta=0, oPhi=0, rPer=24000, seaLevel=64, moon=False):
     if moon:
         block.data += starData.moonHeader % (name, block.ID, aDens, gMul, oDist, oTheta, oPhi, rPer, seaLevel)
@@ -38,6 +38,7 @@ def genPlanetTag(block, name, rings="false", gasGiant="false", aDens=100, gMul=1
         block.data += starData.planetHeader % (name, block.ID, aDens, gMul, oDist, oTheta, oPhi, rPer, seaLevel)
     return block
 
+# generate a planet's data (without the close tag), append it to the block, and return the block
 def genPlanet(block, name, planetNum, numPlanets):
     if random.randint(0,100) > ringsPct:
         rings = "false"
@@ -49,7 +50,6 @@ def genPlanet(block, name, planetNum, numPlanets):
     oDist = int(200*planetNum/(numPlanets+1))
     oTheta = random.randint(minPlanetTheta,maxPlanetTheta)
     oPhi = random.randint(-45,45)
-    
     if oPhi < 0:
         oPhi += 180
 
@@ -61,6 +61,7 @@ def genPlanet(block, name, planetNum, numPlanets):
     block.pSize = gMul
     return block
 
+# generate a moon's data (with close tag), append it to the block, and return the block
 def genMoon(block, name, moonNum, numMoons):
     if random.randint(0,100) > ringsPct:
         rings = "false"
@@ -72,7 +73,6 @@ def genMoon(block, name, moonNum, numMoons):
     oDist = int(200*moonNum/(numMoons+1))
     oTheta = random.randint(minMoonTheta,maxMoonTheta)
     oPhi = random.randint(-45,45)
-
     if oPhi < 0:
         oPhi += 180
 
@@ -84,6 +84,7 @@ def genMoon(block, name, moonNum, numMoons):
     block.ID += 1
     return block
 
+# generate a planet system with moons, append it to the block, and return the block
 def genPlanetSystem(block, name, planetNum, numPlanets, moonNames):
     block = genPlanet(block, name, planetNum, numPlanets)
 
@@ -97,6 +98,7 @@ def genPlanetSystem(block, name, planetNum, numPlanets, moonNames):
     block.data += closePlanetTag
     return block
     
+# generate a star system with planet systems, append it to the block, and return the block
 def genStarSystem(block, name, planetNames):
     temp = random.randint(minStarTemp, maxStarTemp)
     
@@ -108,8 +110,7 @@ def genStarSystem(block, name, planetNames):
 
     size = truncate(random.uniform(minStarSize, maxStarSize), 1)
     
-    numMoons = random.randint(minMoons, maxMoons)
-
+    
     if random.randint(0,100) > blackHolePct:
         blackHole = "false"
     else:
@@ -119,31 +120,42 @@ def genStarSystem(block, name, planetNames):
 
     planetNum = 1
     numPlanets = len(planetNames)
+
+    # generate planets
     for name in planetNames:
+        # pick the number of moons for this planet system
+        numMoons = random.randint(minMoons, maxMoons)
+        # pick moon names
         moonNames = random.sample(starData.planetNames.split("\n"),numMoons)
+        # append data to block
         block = genPlanetSystem(block, name, planetNum, numPlanets, moonNames)
         planetNum += 1
-
+    
     block.data += closeStarTag
 
     return block
 
 
-
+# grab a sample of star names
 starNameList = random.sample(starData.starNames.split("\n"), numStars)
 
+# initialise block with a standard header
 block = dataBlock(starData.standardHeader + closeStarTag, 3)
 
-numPlanets = random.randint(minPlanets,maxPlanets)
-
+# generate the stars
 for name in starNameList:
+    
+    # pick number of planets
+    numPlanets = random.randint(minPlanets,maxPlanets)
+    # grab a sample of planet names
     planetNames = random.sample(starData.planetNames.split("\n"), numPlanets)
+    # append data to block
     block = genStarSystem(block, name, planetNames)
 
-
+# close galaxy tag
 block.data += "\n</galaxy>"
 
-config = open("planetDefs.xml", "w")
+output = open("planetDefs.xml", "w")
 
-config.write(block.data)
-config.close()
+output.write(block.data)
+output.close()
