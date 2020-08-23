0bc58c1ff35b5532b71941382c064235a26d1900
Jake Drake <fingerblasterstudios@gmail.com>
2020-08-22 21:12:27 -0700

    KNOWN BROKEN CODE, DO NOT CREATE ISSUES UNTIL FIXED

--- galaxyPopulator.py
+++ galaxyPopulator.py
@@ -1,5 +1,7 @@
 import random
 import math
+import tkinter as tk
+
 from config import *
 
 def truncate(f):
@@ -197,9 +199,11 @@ class star:
             dimID+=numMoons+1
         return dimID
 
-def genLuna():
+
+
+def genLuna(ID):
     prop=["name", "DIMID"]
-    vals=["Luna", 2]
+    vals=["Luna", ID]
     data = [dataBlock("isKnown", "true"),
             dataBlock("atmosphereDensity", 0),
             dataBlock("gravitationalMultiplier", 17),
@@ -210,18 +214,166 @@ def genLuna():
             dataBlock("seaLevel", 64)]
     return dataBlock("planet", data, prop, vals)
 
+def genVenus(ID):
+    prop=["name", "DIMID"]
+    vals=["Venus", ID]
+    data = [dataBlock("isKnown", "true"),
+            dataBlock("atmosphereDensity", 200),
+            dataBlock("gravitationalMultiplier", 90),
+            dataBlock("orbitalDistance", 30),
+            dataBlock("orbitalTheta", 0),
+            dataBlock("orbitalPhi", 3),
+            dataBlock("rotationalPeriod", 480000),
+            dataBlock("seaLevel", 64),
+            dataBlock("fogColor", "0.9,0.6,0"),
+            dataBlock("skyColor", "0.9,0.6,0"),
+            dataBlock("biomeIds", "advancedrocketry:hotdryrock"),
+            dataBlock("oceanBlock","minecraft:lava")]
+    return dataBlock("planet", data, prop, vals)
+
 def genOverworld():
     prop=["name", "DIMID", "dimMapping"]
     vals=["Earth", 0, ""]
     data = [dataBlock("isKnown", "true"),
             dataBlock("atmosphereDensity", 100),
             dataBlock("gravitationalMultiplier", 100),
-            dataBlock("orbitalDistance", 100),
+            dataBlock("orbitalDistance", 45),
             dataBlock("orbitalTheta", 0),
             dataBlock("orbitalPhi", 23.5),
             dataBlock("rotationalPeriod", 24000),
             dataBlock("seaLevel", 64)]
     return dataBlock("planet", data, prop, vals)
+
+def genMars(ID):
+    prop=["name", "DIMID"]
+    vals=["Mars", ID]
+    data = [dataBlock("isKnown", "true"),
+            dataBlock("atmosphereDensity", 10),
+            dataBlock("gravitationalMultiplier", 38),
+            dataBlock("orbitalDistance", 65),
+            dataBlock("orbitalTheta", 0),
+            dataBlock("orbitalPhi", 2),
+            dataBlock("rotationalPeriod", 24000),
+            dataBlock("seaLevel", 64),
+            dataBlock("biomeIds", "advancedrocketry:hotdryrock"),            
+            dataBlock("fogColor", "0.8,0.2,0"),
+            dataBlock("skyColor", "0.8,0.2,0")]
+    return dataBlock("planet", data, prop, vals)
+
+def genJupiter():
+    prop=["name", "customIcon"]
+    vals=["Jupiter", "gasgiantred"]
+    data = [dataBlock("gasGiant", "true"),
+            dataBlock("atmosphereDensity", 200),
+            dataBlock("gravitationalMultiplier", 180),
+            dataBlock("orbitalDistance", 100),
+            dataBlock("orbitalTheta", 0),
+            dataBlock("orbitalPhi", 1),
+            dataBlock("gas", "hydrogen"),
+            dataBlock("gas", "helium")]
+    return dataBlock("planet", data, prop, vals)
+
+def genEuropa(ID):
+    prop=["name", "DIMID"]
+    vals=["Europa", ID]
+    data = [dataBlock("isKnown", "true"),
+            dataBlock("atmosphereDensity", 0),
+            dataBlock("gravitationalMultiplier", 13),
+            dataBlock("orbitalDistance", 70),
+            dataBlock("orbitalTheta", 0),
+            dataBlock("orbitalPhi", 2),
+            dataBlock("rotationalPeriod", 85000),
+            dataBlock("seaLevel", 64),
+            dataBlock("biomeIds", "frozen_ocean")]
+    return dataBlock("planet", data, prop, vals)
+
+def genSaturn():
+    prop=["name", "customIcon"]
+    vals=["Saturn", "gasgiantred"]
+    data = [dataBlock("gasGiant", "true"),
+            dataBlock("atmosphereDensity", 200),
+            dataBlock("gravitationalMultiplier", 130),
+            dataBlock("orbitalDistance", 140),
+            dataBlock("orbitalTheta", 0),
+            dataBlock("orbitalPhi", 2),
+            dataBlock("hasRings", "true"),
+            dataBlock("ringColor", "0.8,0.7,0.4"),
+            dataBlock("gas", "hydrogen"),
+            dataBlock("gas", "helium")]
+    return dataBlock("planet", data, prop, vals)
+
+def genEnceladus(ID):
+    prop=["name", "DIMID"]
+    vals=["Enceladus", ID]
+    data = [dataBlock("isKnown", "true"),
+            dataBlock("atmosphereDensity", 0),
+            dataBlock("gravitationalMultiplier", 11),
+            dataBlock("orbitalDistance", 75),
+            dataBlock("orbitalTheta", 0),
+            dataBlock("orbitalPhi", 2),
+            dataBlock("rotationalPeriod", 32900),
+            dataBlock("biomeIds", "frozen_ocean")]
+    return dataBlock("planet", data, prop, vals)
+
+def genTitan(ID):
+    prop=["name", "DIMID"]
+    vals=["Titan", ID]
+    data = [dataBlock("isKnown", "true"),
+            dataBlock("atmosphereDensity", 150),
+            dataBlock("gravitationalMultiplier", 14),
+            dataBlock("orbitalDistance", 175),
+            dataBlock("orbitalTheta", 0),
+            dataBlock("orbitalPhi", 2),
+            dataBlock("rotationalPeriod", 95700),
+            dataBlock("seaLevel", 64),
+            dataBlock("biomeIds", "advancedrocketry:hotdryrock"),            
+            dataBlock("fogColor", "0.8,0.6,0.1"),
+            dataBlock("skyColor", "0.8,0.6,0.1")]
+    return dataBlock("planet", data, prop, vals)
+
+def genSol(x, y, temp, size):
+    Sol = star("Sol",
+                 x,
+                 y,
+                 temp,
+                 size,
+                 "false")
+
+    Sol.data.append(planet("Mercury",
+                      2,
+                      0.075,
+                      90,
+                      200,
+                      0,
+                      7,
+                      240000,
+                      16).data)
+
+    Sol.data.append(genVenus(3))
+    
+    Earth = genOverworld()
+    Earth.append(genLuna(4))
+    
+    Sol.data.append(Earth)
+    Sol.data.append(genMars(5))
+    Jupiter = genJupiter()
+    Jupiter.append(planet("Io",
+                          6,
+                          0.2,
+                          18,
+                          0,
+                          0,
+                          0,
+                          42500,
+                          65).data)
+    Jupiter.append(genEuropa(7))
+    Sol.data.append(Jupiter)
+
+    Saturn = genSaturn()
+    Saturn.append(genEnceladus(8))
+    Saturn.append(genTitan(9))
+    Sol.data.append(Saturn)
+    return Sol
 
 def lerp(min, max, pos):
     return pos * (max-min) + min
@@ -258,23 +410,19 @@ def genGalaxy(nStars, nArms, iRad, oRad, startID, sSev, map, firstRun = True, na
 
             temp = int(lerp(minStarTemp, maxStarTemp, i * nArms / nStars)) + random.randint(-minStarTemp / 2, maxStarTemp / 2)
             size = truncate(random.uniform(minStarSize, maxStarSize))
-            print(int(temp/maxStarTemp * len(typeList)))
+            #print(int(temp/maxStarTemp * len(typeList)))
             cat = typeList[int(temp/maxStarTemp) * len(typeList) - 1]
             name ="%s-%s #%s" % (cat, int(size / maxStarSize * 100), i*nArms + arm)
             if sol:
-                name = "Sol"
-            newStar = star(name, x + posX, y + posY, temp, size)
+                newStar = genSol(x + posX, y + posY, temp, size)
+            else:
+                newStar = star(name, x + posX, y + posY, temp, size)
             nSis = random.randint(minStars, maxStars)
 
             if nSis > 0:
                 newStar.genSisters(name, nSis)
             
-            if sol:
-                luna = genLuna()
-                earth = genOverworld()
-                earth.append(luna)
-                newStar.data.append(earth)
-            else:    
+            if not sol:
                 ID = newStar.genPlanets(random.randint(minPlanets, maxPlanets), ID)
             # append data to block
             
@@ -283,8 +431,6 @@ def genGalaxy(nStars, nArms, iRad, oRad, startID, sSev, map, firstRun = True, na
     #return map
 
 def distance(tup1, tup2):
-    print("tup1:")
-    print(tup1)
     x1 = tup1[0]
     y1 = tup1[1]
 
@@ -296,22 +442,25 @@ def distance(tup1, tup2):
 
     return math.sqrt(math.pow(xLen, 2) + math.pow(yLen, 2))
 
-
-map=dataBlock("galaxy")
-galaxyNameList = random.sample(bhList, numGalaxies)
-
-ID = genGalaxy(maxSystems, 4, maxIRad, maxORad, minDIMID, 250, map)
-
-a = random.uniform(-math.pi, math.pi)
+def regen():
+    
 
 
-r = maxGalR
+    map=dataBlock("galaxy")
+    galaxyNameList = random.sample(bhList, numGalaxies)
+    nArms = 4
+    ID = genGalaxy(maxSystems, nArms, maxIRad, maxORad, minDIMID, maxSpirSeverity * nArms, map)
 
-        
-posX = int(math.cos(a) * r)
-posY = int(math.sin(a) * r)
+    a = random.uniform(-math.pi, math.pi)
 
-ID = genGalaxy(minSystems, 7, minIRad, minORad, ID, minSpirSeverity, map, False, "IC 1101*", posX, posY)
+
+    r = maxGalR
+
+            
+    posX = int(math.cos(a) * r)
+    posY = int(math.sin(a) * r)
+
+    ID = genGalaxy(minSystems, 7, minIRad, minORad, ID, minSpirSeverity, map, False, "IC 1101*", posX, posY)
 
         
 """
@@ -350,6 +499,88 @@ for i in range(numGalaxies):
                    posY)
 """     
 
-output = open("planetDefs.xml", "w")
-output.write(map.toXML())
-output.close()
+#output = open("planetDefs.xml", "w")
+#output.write(map.toXML())
+#output.close()
+top = tk.Tk()
+top.resizable(0,0)
+frame = tk.Frame(top, bg="grey", width=1024, height=768)
+frame.pack()
+img = tk.PhotoImage(width = 512, height= 512)
+#for x in range(512):
+#for y in range(512):
+        
+data = [["black" for x in range(512)] for y in range(512)]
+
+
+
+img.put(data, (0,0))
+preview = tk.Label(frame, bd=0, image = img)
+preview.place(x=512)
+
+
+sliderVars={"numArms":["Number of arms: ", #Name
+                        (2,12), #Min, Max values
+                        1,#Steps
+                        2],#Default Value
+            
+            "minDIMID":["Lowest DIMID: ",
+                        (3,27),
+                        1,
+                        3],
+            
+            "spirSeverity":["Spiral Slope: ",
+                            (0,6.28),
+                            0.01,
+                            1.0],
+            
+            "numStars":["Number of Stars",
+                        (20,3000),
+                        10,
+                        10],
+            
+            "iRad":["Inner Radius: ",
+                    (1,50),
+                    0.1,
+                    25],
+            
+            "oRad":["Outer Radius: ",
+                    (50,750),
+                    0.1,
+                    500],
+            "":["",
+                (),
+                ,
+                ]}
+
+sliderLabels=["Number of arms: ","Lowest DIMID: ", "Spiral Slope: ", "Number of Stars: ", "Inner Radius: ", "Outer Radius: "]
+sliderValues=[(2,12),(3, 27), (0,6.28),(20,3000), (1,50),(50,750)]
+sliderSteps=[1, 1, 0.01, 10, 0.1, 0.1]
+
+variables=[]
+
+nArmsInput=tk.Scale(frame, orient=tk.HORIZONTAL,bd=0,fg="darkorange",bg="grey", label="Number of Arms: ", from_=2, to=12, resolution=1, variable=numArms)
+nArmsInput.place(x=5, y=5)
+
+mDIDInput=tk.Scale(frame, orient=tk.HORIZONTAL,bd=0,fg="darkorange",bg="grey", label="Lowest DIMID: ", from_=3, to=27, resolution=1, variable=minDIMID)
+mDIDInput.place(x=5, y=70)
+
+spirSevInput=tk.Scale(frame, orient=tk.HORIZONTAL,bd=0,fg="darkorange",bg="grey", label="Spiral Slope: ", from_=0, to=6.28, resolution=0.01, variable=spirSeverity, length=380)
+spirSevInput.place(x=120, y=5)
+
+nStarsInput=tk.Scale(frame, orient=tk.HORIZONTAL,bd=0,fg="darkorange",bg="grey", label="Number of Stars: ", from_=20, to=3000, resolution=10, variable=numStars, length=380)
+nStarsInput.place(x=120, y=70)
+
+iRadInput=tk.Scale(frame, orient=tk.HORIZONTAL,bd=0,fg="darkorange",bg="grey", label="Inner Radius: ", from_=1, to=50, resolution=0.1, variable=iRad, length=495)
+iRadInput.place(x=5, y=135)
+
+iRadInput=tk.Scale(frame, orient=tk.HORIZONTAL,bd=0,fg="darkorange",bg="grey", label="Outer Radius: ", from_=50, to=750, resolution=0.1, variable=oRad, length=495)
+iRadInput.place(x=5, y=200)
+
+genBut=tk.Button(frame, bd=1, fg="darkorange",bg="grey", text="Regenerate", activeforeground="orange", activebackground="grey")
+genBut.place(x=5, y=740)
+
+savBut=tk.Button(frame, bd=1, fg="darkorange",bg="grey", text="Save", activeforeground="orange", activebackground="grey")
+savBut.place(x=75, y=740)
+
+top.mainloop()
